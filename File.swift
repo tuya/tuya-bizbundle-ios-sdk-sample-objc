b2830d5c94eafc3b42092ab8d838f733e00b1ee1
caili <caili@tuya.com>
2023-07-12 18:12:46 +0800

    feat: 验证 swift 导入是否正常

---
+++ File.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  tuya-bizbundle-ios-sample-objc
+//
+//  Created by 尼奥 on 2023/5/9.
+//  Copyright © 2023 Tuya. All rights reserved.
+//
+
+import Foundation
+import ThingModuleServices
